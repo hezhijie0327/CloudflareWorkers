07abbe0dfa1b54a77a5fd8554f22c48680d35247
Zhijie He <hezhijie0327@hotmail.com>
2021-02-16 16:46:47 +0800

    proxy.js | Version 1.0.4
    1. <Change> Enhance performance.
        /*
        (
            (
                proxy_config.access.allowed.country.length === 0
                ||
                (
                    proxy_config.access.allowed.country.length !== 0
                    &&
                    proxy_config.access.allowed.country.includes(proxy_config.access.current.country)
                )
            )
            &&
            (
                proxy_config.access.allowed.ip.length === 0
                ||
                (
                    proxy_config.access.allowed.ip.length !== 0
                    &&
                    proxy_config.access.allowed.ip.includes(proxy_config.access.current.ip)
                )
            )
            &&
            (
                proxy_config.access.blocked.country.length === 0
                ||
                (
                    proxy_config.access.blocked.country.length !== 0
                    &&
                    (
                        proxy_config.access.blocked.country.includes(proxy_config.access.current.country) === false
                        ||
                        (
                            proxy_config.access.allowed.country.includes(proxy_config.access.current.country)
                            &&
                            proxy_config.access.blocked.country.includes(proxy_config.access.current.country)
                        )
                    )
                )
            )
            &&
            (
                proxy_config.access.blocked.ip.length === 0
                ||
                (
                    proxy_config.access.blocked.ip.length !== 0
                    &&
                    (
                        proxy_config.access.blocked.ip.includes(proxy_config.access.current.ip) === false
                        ||
                        (
                            proxy_config.access.allowed.ip.includes(proxy_config.access.current.ip)
                            &&
                            proxy_config.access.blocked.ip.includes(proxy_config.access.current.ip)
                        )
                    )
                )
            )
        )
        */

--- proxy.js
+++ proxy.js
@@ -1,4 +1,4 @@
-// Current Version: 1.0.3
+// Current Version: 1.0.4
 // Description: Using Cloudflare Workers to reverse proxy website.
 
 addEventListener("fetch", (event) => {
@@ -35,7 +35,7 @@ async function handleRequest(request) {
         path: "",
         protocol: "https",
     };
-    if (((proxy_config.access.allowed.country.length === 0 || (proxy_config.access.allowed.country.length !== 0 && proxy_config.access.allowed.country.includes(proxy_config.access.current.country))) && (proxy_config.access.allowed.ip.length === 0 || (proxy_config.access.allowed.ip.length !== 0 && proxy_config.access.allowed.ip.includes(proxy_config.access.current.ip))) && (proxy_config.access.blocked.country.length === 0 || (proxy_config.access.blocked.country.length !== 0 && proxy_config.access.blocked.country.includes(proxy_config.access.current.country) === false)) && (proxy_config.access.blocked.ip.length === 0 || (proxy_config.access.blocked.ip.length !== 0 && proxy_config.access.blocked.ip.includes(proxy_config.access.current.ip) === false))) || (proxy_config.access.allowed.country.includes(proxy_config.access.current.country) && proxy_config.access.blocked.country.includes(proxy_config.access.current.country)) || (proxy_config.access.allowed.ip.includes(proxy_config.access.current.ip) && proxy_config.access.blocked.ip.includes(proxy_config.access.current.ip))) {
+    if ((proxy_config.access.allowed.country.length === 0 || (proxy_config.access.allowed.country.length !== 0 && proxy_config.access.allowed.country.includes(proxy_config.access.current.country))) && (proxy_config.access.allowed.ip.length === 0 || (proxy_config.access.allowed.ip.length !== 0 && proxy_config.access.allowed.ip.includes(proxy_config.access.current.ip))) && (proxy_config.access.blocked.country.length === 0 || (proxy_config.access.blocked.country.length !== 0 && (proxy_config.access.blocked.country.includes(proxy_config.access.current.country) === false || (proxy_config.access.allowed.country.includes(proxy_config.access.current.country) && proxy_config.access.blocked.country.includes(proxy_config.access.current.country))))) && (proxy_config.access.blocked.ip.length === 0 || (proxy_config.access.blocked.ip.length !== 0 && (proxy_config.access.blocked.ip.includes(proxy_config.access.current.ip) === false || (proxy_config.access.allowed.ip.includes(proxy_config.access.current.ip) && proxy_config.access.blocked.ip.includes(proxy_config.access.current.ip)))))) {
         const proxy_url = new URL(proxy_config.protocol + "://" + proxy_config.host + "/" + proxy_config.path);
         const request_url = new URL(request.url);
         request_url.protocol = proxy_url.protocol;
