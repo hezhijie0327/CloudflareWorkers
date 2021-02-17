21989352d4418111817ecb671c87eac71539e44c
Zhijie He <hezhijie0327@hotmail.com>
2021-02-17 09:20:12 +0800

    github.js | Version 1.0.3
    1. <Change> Enhance performance.

--- github.js
+++ github.js
@@ -1,4 +1,4 @@
-// Current Version: 1.0.2
+// Current Version: 1.0.3
 // Description: Using Cloudflare Workers to speed up github.com's visting.
 
 addEventListener("fetch", (event) => {
@@ -21,17 +21,17 @@ async function handleRequest(request) {
         return Response.redirect("https://" + path[0] + "/" + url.replace(/^https\:\/\/(desktop\.githubusercontent\.com|github\-releases\.githubusercontent\.com|github\.com|raw\.githubusercontent\.com|user\-images\.githubusercontent\.com)\//gim, ""), 301);
     }
     var response = "";
-    var response_archive_blob_edit_raw_release = await fetch("https://github.com/" + url);
+    var response_archive_blob_clone_edit_raw_release = await fetch("https://github.com/" + url);
     var response_desktop = await fetch("https://desktop.githubusercontent.com/" + url);
     var response_image = await fetch("https://user-images.githubusercontent.com/" + url);
     var response_raw = await fetch("https://raw.githubusercontent.com/" + url);
     var response_release = await fetch("https://github-releases.githubusercontent.com/" + url);
-    if (response_archive_blob_edit_raw_release.status === 200) {
+    if (response_archive_blob_clone_edit_raw_release.status === 200) {
         if (path[2].endsWith(".git")) {
             return Response.redirect("https://" + clone_url[Math.floor(Math.random() * clone_url.length)] + "/" + url, 302);
         } else {
             if (path[3] === "archive" || (path[3] === "releases" && path[4] === "download")) {
-                response = response_archive_blob_edit_raw_release;
+                response = response_archive_blob_clone_edit_raw_release;
             } else if (path[3] === "blob" || path[3] === "edit" || path[3] === "raw") {
                 for (var i = 0; i < path.length; i++) {
                     if (i === 0) {
