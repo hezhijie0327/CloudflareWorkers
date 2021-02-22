6ce262e2e6a4739960b95611b89eccbd3a8c1ea8
Zhijie He <hezhijie0327@hotmail.com>
2021-02-22 10:45:44 +0800

    github.js | Version 1.0.7
    1. <Change> Enhance performance.

--- github.js
+++ github.js
@@ -1,4 +1,4 @@
-// Current Version: 1.0.6
+// Current Version: 1.0.7
 // Description: Using Cloudflare Workers to speed up github.com's visting.
 
 addEventListener("fetch", (event) => {
@@ -20,47 +20,49 @@ async function handleRequest(request) {
     let url = request.url.substr(8);
     path = url.split("/");
     url = url.substr(url.indexOf("/") + 1);
-    if (url.startsWith("https://")) {
-        return Response.redirect("https://" + path[0] + "/" + url.replace(/^https\:\/\/(codeload\.github\.com|desktop\.githubusercontent\.com|github\-releases\.githubusercontent\.com|github\.com|raw\.githubusercontent\.com|user\-images\.githubusercontent\.com)\//gim, ""), 301);
-    }
     var response = "";
-    var response_archive_blob_clone_edit_raw_release = await fetch("https://github.com/" + url);
-    var response_codeload = await fetch("https://codeload.github.com/" + url);
-    var response_desktop = await fetch("https://desktop.githubusercontent.com/" + url);
-    var response_image = await fetch("https://user-images.githubusercontent.com/" + url);
-    var response_raw = await fetch("https://raw.githubusercontent.com/" + url);
-    var response_release = await fetch("https://github-releases.githubusercontent.com/" + url);
-    if (response_archive_blob_clone_edit_raw_release.status === 200) {
-        if (path[2].endsWith(".git")) {
-            var mirror_url = mirror.private.concat(mirror.public);
-            var redirect = mirror_url[Math.floor(Math.random() * mirror_url.length)];
-            return Response.redirect("https://" + redirect + "/" + url, 302);
-        } else {
-            if (path[3] === "archive" || (path[3] === "releases" && path[4] === "download")) {
-                response = response_archive_blob_clone_edit_raw_release;
-            } else if (path[3] === "blob" || path[3] === "edit" || path[3] === "raw") {
-                for (var i = 0; i < path.length; i++) {
-                    if (i === 0) {
-                        url = path[i];
-                    } else if (i === 3) {
-                        url = url;
-                    } else {
-                        url = url + "/" + path[i];
-                    }
-                }
-                return Response.redirect("https://" + url, 301);
-            }
+    if (url !== "") {
+        if (url.startsWith("https://")) {
+            return Response.redirect("https://" + path[0] + "/" + url.replace(/^https\:\/\/(codeload\.github\.com|desktop\.githubusercontent\.com|github\-releases\.githubusercontent\.com|github\.com|raw\.githubusercontent\.com|user\-images\.githubusercontent\.com)\//gim, ""), 301);
         }
-    } else if (response_codeload.status === 200) {
-        response = response_codeload;
-    } else if (response_desktop.status === 200) {
-        response = response_desktop;
-    } else if (response_image.status === 200) {
-        response = response_image;
-    } else if (response_raw.status === 200) {
-        response = response_raw;
-    } else if (response_release.status === 200) {
-        response = response_release;
+        var response_archive_blob_clone_edit_raw_release = await fetch("https://github.com/" + url);
+        var response_codeload = await fetch("https://codeload.github.com/" + url);
+        var response_desktop = await fetch("https://desktop.githubusercontent.com/" + url);
+        var response_image = await fetch("https://user-images.githubusercontent.com/" + url);
+        var response_raw = await fetch("https://raw.githubusercontent.com/" + url);
+        var response_release = await fetch("https://github-releases.githubusercontent.com/" + url);
+        if (response_archive_blob_clone_edit_raw_release.status === 200) {
+            if (path[2].endsWith(".git")) {
+                var mirror_url = mirror.private.concat(mirror.public);
+                var redirect = mirror_url[Math.floor(Math.random() * mirror_url.length)];
+                return Response.redirect("https://" + redirect + "/" + url, 302);
+            } else {
+                if (path[3] === "archive" || (path[3] === "releases" && path[4] === "download")) {
+                    response = response_archive_blob_clone_edit_raw_release;
+                } else if (path[3] === "blob" || path[3] === "edit" || path[3] === "raw") {
+                    for (var i = 0; i < path.length; i++) {
+                        if (i === 0) {
+                            url = path[i];
+                        } else if (i === 3) {
+                            url = url;
+                        } else {
+                            url = url + "/" + path[i];
+                        }
+                    }
+                    return Response.redirect("https://" + url, 301);
+                }
+            }
+        } else if (response_codeload.status === 200) {
+            response = response_codeload;
+        } else if (response_desktop.status === 200) {
+            response = response_desktop;
+        } else if (response_image.status === 200) {
+            response = response_image;
+        } else if (response_raw.status === 200) {
+            response = response_raw;
+        } else if (response_release.status === 200) {
+            response = response_release;
+        }
     }
     if (response !== "") {
         return new Response(response.body, {
