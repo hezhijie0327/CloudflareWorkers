3f4bf38308749da4ac1a4934de5943b231ed5530
Zhijie He <hezhijie0327@hotmail.com>
2022-02-11 21:03:12 +0800

    gpg.js | Version 1.1.4
    1. <Change> Format code.

--- gpg.js
+++ gpg.js
@@ -1,57 +1,63 @@
-// Current Version: 1.1.3
+// Current Version: 1.1.4
 // Description: Using Cloudflare Workers to backup your GPG key.
 
-addEventListener("fetch", (event) => {
-    event.respondWith(handleRequest(event.request));
-});
+addEventListener( "fetch", ( event ) =>
+{
+    event.respondWith( handleRequest( event.request ) )
+} )
 
-async function handleRequest(request) {
-    let url = request.url.substr(8);
-    path = url.split("/");
-    url = url.substr(url.indexOf("/") + 1);
+async function handleRequest ( request )
+{
+    let url = request.url.substr( 8 )
+    path = url.split( "/" )
+    url = url.substr( url.indexOf( "/" ) + 1 )
     const gpg = {
         info: {
-            private: "curl -fsSL 'https://" + path[0] + "/" + url + "' | jq -r '.key.private' | base64 -d | gpg --batch --import --passphrase '<PASSWORD>'",
-            public: "curl -fsSL 'https://" + path[0] + "/" + url + "' | jq -r '.key.public' | base64 -d | gpg --batch --import",
+            private: "curl -fsSL 'https://" + path[ 0 ] + "/" + url + "' | jq -r '.key.private' | base64 -d | gpg --batch --import --passphrase '<PASSWORD>'",
+            public: "curl -fsSL 'https://" + path[ 0 ] + "/" + url + "' | jq -r '.key.public' | base64 -d | gpg --batch --import",
             trust: "Z3BnIC0tbGlzdC1rZXlzIC0tZmluZ2VycHJpbnQgXAogICAgfCBncmVwICdecHViJyAtQSAxIFwKICAgIHwgdGFpbCAtbiAxIFwKICAgIHwgdHIgLWQgJyAnIFwKICAgIHwgYXdrICdCRUdJTiB7IEZTID0gIlxuIiB9OyB7IHByaW50ICQxIjo2OiIgfScgXAogICAgfCBncGcgLS1pbXBvcnQtb3duZXJ0cnVzdAo=",
         },
         key: {
             private: "",
             public: "",
         },
-        secret: btoa(""),
-    };
-    if (url === atob(gpg.secret) + "/import.json") {
-        return new Response(JSON.stringify(gpg, null, 2), {
+        secret: btoa( "" ),
+    }
+    if ( url === atob( gpg.secret ) + "/import.json" )
+    {
+        return new Response( JSON.stringify( gpg, null, 2 ), {
             status: 200,
             headers: {
                 "Access-Control-Allow-Origin": "*",
                 "content-type": "application/json;charset=UTF-8",
             },
-        });
-    } else if (url === atob(gpg.secret) + "/private.txt") {
-        return new Response(atob(gpg.key.private), {
+        } )
+    } else if ( url === atob( gpg.secret ) + "/private.txt" )
+    {
+        return new Response( atob( gpg.key.private ), {
             status: 200,
             headers: {
                 "Access-Control-Allow-Origin": "*",
                 "content-type": "text/plain;charset=UTF-8",
             },
-        });
-    } else if (url === atob(gpg.secret) + "/public.txt") {
-        return new Response(atob(gpg.key.public), {
+        } )
+    } else if ( url === atob( gpg.secret ) + "/public.txt" )
+    {
+        return new Response( atob( gpg.key.public ), {
             status: 200,
             headers: {
                 "Access-Control-Allow-Origin": "*",
                 "content-type": "text/plain;charset=UTF-8",
             },
-        });
-    } else {
-        return new Response("404 Not Found", {
+        } )
+    } else
+    {
+        return new Response( "404 Not Found", {
             status: 404,
             headers: {
                 "Access-Control-Allow-Origin": "*",
                 "content-type": "text/plain;charset=UTF-8",
             },
-        });
+        } )
     }
 }
