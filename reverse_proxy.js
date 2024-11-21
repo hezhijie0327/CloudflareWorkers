b374489f9723bf12a08efd7897339285d9f383be
Zhijie He <hezhijie0327@hotmail.com>
2024-11-21 22:59:20 +0800

    Update reverse_proxy.js

--- reverse_proxy.js
+++ reverse_proxy.js
@@ -5,33 +5,42 @@ addEventListener( 'fetch', e => e.respondWith( fetchHandler( e ) ) )
 
 async function fetchHandler ( e )
 {
-    const req = e.request
-    const urlObj = new URL( req.url )
-    const targetUrl = urlObj.searchParams.get( 'q' )
-        ? `https://${ urlObj.host }/${ urlObj.searchParams.get( 'q' ) }`
-        : urlObj.href.substring( urlObj.origin.length + 1 ).replace( /^https?:\/+/, 'https://' )
-
-    const res = await fetch( targetUrl, { body: req.body, headers: req.headers, method: req.method, redirect: 'manual' } )
-    const resHdr = new Headers( res.headers )
-
-    if ( resHdr.has( 'Location' ) )
+    try
     {
-        return fetchHandler( { request: new Request( resHdr.get( 'Location' ), { ...req, redirect: 'manual' } ) } )
-    }
+        const req = e.request
+        const urlObj = new URL( req.url )
+        const targetUrl = urlObj.searchParams.get( 'q' )
+            ? `https://${ urlObj.host }/${ urlObj.searchParams.get( 'q' ) }`
+            : urlObj.href.substring( urlObj.origin.length + 1 ).replace( /^https?:\/+/, 'https://' )
 
-    resHdr.set( 'Access-Control-Allow-Headers', '*' )
-    resHdr.set( 'Access-Control-Allow-Methods', '*' )
-    resHdr.set( 'Access-Control-Allow-Origin', '*' )
-    resHdr.set( 'Cache-Control', 'no-store' )
+        const res = await fetch( targetUrl, { body: req.body, headers: req.headers, method: req.method, redirect: 'manual' } )
+        const resHdr = new Headers( res.headers )
 
-    if ( resHdr.get( "Content-Type" )?.includes( "text/html" ) )
+        if ( resHdr.has( 'Location' ) )
+        {
+            return fetchHandler( { request: new Request( resHdr.get( 'Location' ), { ...req, redirect: 'manual' } ) } )
+        }
+
+        resHdr.set( 'Access-Control-Allow-Headers', '*' )
+        resHdr.set( 'Access-Control-Allow-Methods', '*' )
+        resHdr.set( 'Access-Control-Allow-Origin', '*' )
+        resHdr.set( 'Cache-Control', 'no-store' )
+
+        if ( resHdr.get( "Content-Type" )?.includes( "text/html" ) )
+        {
+            const body = ( await res.text() ).replace(
+                /((href|src|action)=["'])\/(?!\/)/g,
+                `$1${ urlObj.protocol }//${ urlObj.host }/${ new URL( targetUrl ).origin }/`
+            )
+            return new Response( body, { headers: resHdr, status: res.status } )
+        }
+
+        return new Response( res.body, { headers: resHdr, status: res.status } )
+    } catch ( error )
     {
-        const body = ( await res.text() ).replace(
-            /((href|src|action)=["'])\/(?!\/)/g,
-            `$1${ urlObj.protocol }//${ urlObj.host }/${ new URL( targetUrl ).origin }/`
-        )
-        return new Response( body, { headers: resHdr, status: res.status } )
+        return new Response( JSON.stringify( { error: error.message } ), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json; charset=utf-8' }
+        } )
     }
-
-    return new Response( res.body, { headers: resHdr, status: res.status } )
 }
