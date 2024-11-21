69cfb9992921937870a63770ccbd54ca112e5619
Zhijie He <hezhijie0327@hotmail.com>
2024-11-21 23:59:14 +0800

    docker.js | Version 1.0.6

--- mirror/docker.js
+++ mirror/docker.js
@@ -1,161 +1,48 @@
-// Current Version: 1.0.5
+// Current Version: 1.0.6
 // Description: Using Cloudflare Workers to speed up registry-1.docker.io's visting.
 
-addEventListener( 'fetch', e =>
-{
-    const ret = fetchHandler( e )
-        .catch( err => makeRes( 'Error:\n' + err.stack, {}, 502 ) )
-    e.respondWith( ret )
-} )
-
-function makeRes ( body, headers = {}, status = 200 )
-{
-    headers[ 'Access-Control-Allow-Origin' ] = '*'
-    return new Response( body, { headers, status } )
-}
-
-function newUrl ( urlStr )
-{
-    try
-    {
-        return new URL( urlStr )
-    } catch ( err )
-    {
-        return null
-    }
-}
+addEventListener( 'fetch', e => e.respondWith( fetchHandler( e ) ) )
 
 async function fetchHandler ( e )
 {
-    const getReqHeader = ( key ) => e.request.headers.get( key )
-
-    let parameter = {
-        headers: {
-            'Accept': getReqHeader( "Accept" ),
-            'Accept-Encoding': getReqHeader( "Accept-Encoding" ),
-            'Accept-Language': getReqHeader( "Accept-Language" ),
-            'Cache-Control': 'max-age=0',
-            'Connection': 'keep-alive',
-            'Host': 'registry-1.docker.io',
-            'User-Agent': getReqHeader( "User-Agent" ),
-        }
-    }
-    let url = new URL( e.request.url )
+    const url = new URL( e.request.url )
     url.hostname = 'registry-1.docker.io'
-    let workers_url = e.request.url.substr( 8 )
-    workers_url = workers_url.split( "/" )
 
     if ( url.pathname === '/token' )
     {
-        let visiting_ip = e.request.headers.get( 'CF-Connecting-IP' )
+        const ip = e.request.headers.get( 'CF-Connecting-IP' ) || '127.0.0.1'
+        const authHostname = /^(\d{1,3}\.){3}\d{1,3}$/.test( ip )
+            ? ( Math.random() < 0.5 ? 'auth.docker.com' : 'auth.docker.io' )
+            : 'auth.ipv6.docker.com'
 
-        if ( visiting_ip === null )
-        {
-            visiting_ip = '127.0.0.1'
-        }
-
-        if ( visiting_ip.search( /^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/i ) === 0 )
-        {
-            switch ( Math.floor( Math.random() * 2 ) )
-            {
-                case 0:
-                    var auth_hostname = 'auth.docker.com'
-                case 1:
-                    var auth_hostname = 'auth.docker.io'
-            }
-        } else
-        {
-            var auth_hostname = 'auth.ipv6.docker.com'
-        }
-
-        return fetch( new Request( 'https://' + auth_hostname + url.pathname + url.search, e.request ) )
+        return fetch( new Request( `https://${ authHostname }${ url.pathname }${ url.search }`, e.request ) )
     }
 
-    if ( e.request.headers.has( "Authorization" ) )
+    const headers = {
+        'Host': url.hostname,
+        ...( e.request.headers.has( "Authorization" ) && { Authorization: e.request.headers.get( "Authorization" ) } )
+    }
+
+    let response = await fetch( new Request( url, { headers } ), e.request )
+    let tempHeaders = new Headers( response.headers )
+
+    if ( tempHeaders.get( "WWW-Authenticate" ) )
     {
-        parameter.headers.Authorization = getReqHeader( "Authorization" )
+        tempHeaders.set( "WWW-Authenticate", tempHeaders.get( "WWW-Authenticate" ).replace( /https:\/\/auth\.(ipv6\.)?docker\.(io|com)/g, `https://${ e.request.url.split( '/' )[ 2 ] }` ) )
     }
 
-    let response = await fetch( new Request( url, e.request ), parameter )
-    let temp_headers = new Headers( response.headers )
-
-    if ( temp_headers.get( "WWW-Authenticate" ) )
+    if ( tempHeaders.get( "Location" ) )
     {
-        temp_headers.set( "WWW-Authenticate", response.headers.get( "WWW-Authenticate" ).replace( new RegExp( 'https://auth.(ipv6.)?docker.(io|com)', 'g' ), 'https://' + workers_url[ 0 ] ) )
+        const res = await fetch( new Request( tempHeaders.get( "Location" ), { body: e.request.body, headers: e.request.headers, method: e.request.method, redirect: 'follow' } ) )
+        const resHdrNew = new Headers( res.headers )
+
+        resHdrNew.set( 'Access-Control-Allow-Headers', '*' )
+        resHdrNew.set( 'Access-Control-Allow-Methods', '*' )
+        resHdrNew.set( 'Access-Control-Allow-Origin', '*' )
+        resHdrNew.set( 'Cache-Control', 'no-store' )
+
+        return new Response( res.body, { headers: resHdrNew, status: res.status } )
     }
 
-    if ( temp_headers.get( "Location" ) )
-    {
-        return httpHandler( e.request, temp_headers.get( "Location" ) )
-    }
-
-    return new Response( response.body, {
-        status: response.status,
-        headers: temp_headers
-    } )
-
-}
-
-function httpHandler ( req, pathname )
-{
-    const reqHdrRaw = req.headers
-
-    if ( req.method === 'OPTIONS' && reqHdrRaw.has( 'Access-Control-Request-Headers' ) )
-    {
-        return new Response( null, {
-            headers: {
-                'Access-Control-Allow-Methods': 'DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Max-Age': '86400',
-            }, status: 204
-        } )
-    }
-
-    let rawLen = ''
-    let urlStr = pathname
-
-    const reqHdrNew = new Headers( reqHdrRaw )
-    const urlObj = newUrl( urlStr )
-    const reqInit = {
-        body: req.body,
-        headers: reqHdrNew,
-        method: req.method,
-        redirect: 'follow'
-    }
-
-    return proxy( rawLen, reqInit, urlObj )
-}
-
-async function proxy ( rawLen, reqInit, urlObj )
-{
-    const res = await fetch( urlObj.href, reqInit )
-    const resHdrOld = res.headers
-    const resHdrNew = new Headers( resHdrOld )
-
-    if ( rawLen )
-    {
-        const newLen = resHdrOld.get( 'Content-Length' ) || ''
-        const badLen = ( rawLen !== newLen )
-
-        if ( badLen )
-        {
-            return makeRes( res.body, {
-                '--error': `bad len: ${ newLen }, except: ${ rawLen }`,
-                'Access-Control-Expose-Headers': '--error',
-            }, 400 )
-        }
-    }
-
-    resHdrNew.set( 'Access-Control-Allow-Origin', '*' )
-    resHdrNew.set( 'Access-Control-Expose-Headers', '*' )
-    resHdrNew.set( 'Cache-Control', 'max-age=0' )
-
-    resHdrNew.delete( 'Clear-Site-Data' )
-    resHdrNew.delete( 'Content-Security-Policy' )
-    resHdrNew.delete( 'Content-Security-Policy-Report-Only' )
-
-    return new Response( res.body, {
-        headers: resHdrNew,
-        status: res.status
-    } )
+    return new Response( response.body, { status: response.status, headers: tempHeaders } )
 }
