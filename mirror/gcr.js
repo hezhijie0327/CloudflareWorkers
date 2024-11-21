42dd52213483465a79da9a53a23bfe3211af496e
Zhijie He <hezhijie0327@hotmail.com>
2024-11-22 00:30:15 +0800

    gcr.js | Version 1.0.5

--- mirror/gcr.js
+++ mirror/gcr.js
@@ -1,4 +1,4 @@
-// Current Version: 1.0.4
+// Current Version: 1.0.5
 // Description: Using Cloudflare Workers to speed up gcr.io's visiting.
 
 addEventListener( 'fetch', e => e.respondWith( fetchHandler( e ) ) )
@@ -15,12 +15,24 @@ async function fetchHandler ( e )
         }
     } ), e.request )
 
-    if ( response.headers.get( "WWW-Authenticate" ) )
+    let tempHeaders = new Headers( response.headers )
+
+    if ( tempHeaders.get( "WWW-Authenticate" ) )
     {
-        response.headers.set( "WWW-Authenticate", response.headers.get( "WWW-Authenticate" ).replace(
-            /https:\/\/gcr\.io/g,
-            `https://${ e.request.url.split( '/' )[ 2 ] }`
-        ) )
+        tempHeaders.set( "WWW-Authenticate", tempHeaders.get( "WWW-Authenticate" ).replace( /https:\/\/ghci\.io/g, `https://${ e.request.url.split( '/' )[ 2 ] }` ) )
+    }
+
+    if ( tempHeaders.get( "Location" ) )
+    {
+        const res = await fetch( new Request( tempHeaders.get( "Location" ), { body: e.request.body, headers: e.request.headers, method: e.request.method, redirect: 'follow' } ) )
+        const resHdrNew = new Headers( res.headers )
+
+        resHdrNew.set( 'Access-Control-Allow-Headers', '*' )
+        resHdrNew.set( 'Access-Control-Allow-Methods', '*' )
+        resHdrNew.set( 'Access-Control-Allow-Origin', '*' )
+        resHdrNew.set( 'Cache-Control', 'no-store' )
+
+        return new Response( res.body, { headers: resHdrNew, status: res.status } )
     }
 
     return new Response( response.body, { status: response.status, headers: response.headers } )
