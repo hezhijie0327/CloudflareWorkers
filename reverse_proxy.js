3f4b584b3148a9b71354a98f6b8e48931300fff8
Zhijie He <hezhijie0327@hotmail.com>
2024-11-21 22:56:57 +0800

    Update reverse_proxy.js

--- reverse_proxy.js
+++ reverse_proxy.js
@@ -1,75 +1,37 @@
-// Current Version: 1.1.2
+// Current Version: 1.1.3
 // Description: Using Cloudflare Workers to Reverse Proxy everything.
 
-addEventListener( 'fetch', event =>
+addEventListener( 'fetch', e => e.respondWith( fetchHandler( e ) ) )
+
+async function fetchHandler ( e )
 {
-    event.respondWith( handleRequest( event.request ) )
-} )
+    const req = e.request
+    const urlObj = new URL( req.url )
+    const targetUrl = urlObj.searchParams.get( 'q' )
+        ? `https://${ urlObj.host }/${ urlObj.searchParams.get( 'q' ) }`
+        : urlObj.href.substring( urlObj.origin.length + 1 ).replace( /^https?:\/+/, 'https://' )
 
-async function handleRequest ( request )
-{
-    try
+    const res = await fetch( targetUrl, { body: req.body, headers: req.headers, method: req.method, redirect: 'manual' } )
+    const resHdr = new Headers( res.headers )
+
+    if ( resHdr.has( 'Location' ) )
     {
-        const url = new URL( request.url )
-
-        const urlStr = request.url
-        const urlObj = new URL( urlStr )
-
-        let path = decodeURIComponent(
-            urlObj.href.substring( urlObj.origin.length + 1 )
-        ).replace( /^https?:\/+/, 'https://' )
-
-        const targetUrl = path + urlObj.search + urlObj.hash
-
-        const headers = new Headers( [ ...request.headers ].filter( ( [ name ] ) => !name.startsWith( 'cf-' ) ) )
-
-        const response = await fetch( new Request( targetUrl, {
-            headers,
-            method: request.method,
-            body: request.body,
-            redirect: 'manual'
-        } ) )
-
-        let body = response.body
-
-        if ( [ 301, 302, 303, 307, 308 ].includes( response.status ) )
-        {
-            const location = response.headers.get( 'location' )
-            return new Response( body, {
-                status: response.status,
-                headers: {
-                    ...response.headers,
-                    'Location': `/${ encodeURIComponent( location ) }`
-                }
-            } )
-        }
-
-        if ( response.headers.get( "Content-Type" )?.includes( "text/html" ) )
-        {
-            const originalText = await response.text()
-            body = originalText.replace(
-                /((href|src|action)=["'])\/(?!\/)/g,
-                `$1${ url.protocol }//${ url.host }/${ new URL( targetUrl ).origin }/`
-            )
-        }
-
-        const modifiedResponse = new Response( body, {
-            status: response.status,
-            statusText: response.statusText,
-            headers: response.headers
-        } )
-
-        modifiedResponse.headers.set( 'Access-Control-Allow-Headers', '*' )
-        modifiedResponse.headers.set( 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE' )
-        modifiedResponse.headers.set( 'Access-Control-Allow-Origin', '*' )
-        modifiedResponse.headers.set( 'Cache-Control', 'no-store' )
-
-        return modifiedResponse
-    } catch ( error )
-    {
-        return new Response( JSON.stringify( { error: error.message } ), {
-            status: 500,
-            headers: { 'Content-Type': 'application/json; charset=utf-8' }
-        } )
+        return fetchHandler( { request: new Request( resHdr.get( 'Location' ), { ...req, redirect: 'manual' } ) } )
     }
+
+    resHdr.set( 'Access-Control-Allow-Headers', '*' )
+    resHdr.set( 'Access-Control-Allow-Methods', '*' )
+    resHdr.set( 'Access-Control-Allow-Origin', '*' )
+    resHdr.set( 'Cache-Control', 'no-store' )
+
+    if ( resHdr.get( "Content-Type" )?.includes( "text/html" ) )
+    {
+        const body = ( await res.text() ).replace(
+            /((href|src|action)=["'])\/(?!\/)/g,
+            `$1${ urlObj.protocol }//${ urlObj.host }/${ new URL( targetUrl ).origin }/`
+        )
+        return new Response( body, { headers: resHdr, status: res.status } )
+    }
+
+    return new Response( res.body, { headers: resHdr, status: res.status } )
 }
