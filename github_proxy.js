a7782492db3276b259be84b68a188e362ed8ab93
Zhijie He <hezhijie0327@hotmail.com>
2024-11-21 22:33:09 +0800

    github_proxy.js | Version 1.1.3

--- github_proxy.js
+++ github_proxy.js
@@ -1,21 +1,9 @@
-// Current Version: 1.1.2
+// Current Version: 1.1.3
 // Description: Using Cloudflare Workers to proxy GitHub.
-
-const URL_REGEX = {
-    regex_0: /^(?:https?:\/\/)?(((?:codeload|gist)?(?:\.)?github\.com)|(?:desktop|gist|github\-releases|raw|user\-images)?(?:\.)?(githubusercontent\.com))\/.*$/i,
-    regex_1: /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:releases|archive)\/.*$/i,
-    regex_2: /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:blob|edit|raw|suites)\/.*$/i,
-    regex_3: /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/(?:info|git-).*$/i,
-    regex_4: /^(?:https?:\/\/)?raw\.(?:githubusercontent|github)\.com\/.+?\/.+?\/.+?\/.+$/i,
-    regex_5: /^(?:https?:\/\/)?gist\.(?:githubusercontent|github)\.com\/.+?\/.+?\/.+$/i,
-    regex_6: /^(?:https?:\/\/)?github\.com\/.+?\/.+?\/tags.*$/i
-}
 
 addEventListener( 'fetch', e =>
 {
-    const ret = fetchHandler( e )
-        .catch( err => makeRes( 'Error:\n' + err.stack, {}, 502 ) )
-    e.respondWith( ret )
+    e.respondWith( fetchHandler( e ).catch( err => makeRes( 'Error:\n' + err.stack, {}, 502 ) ) )
 } )
 
 function makeRes ( body, headers = {}, status = 200 )
@@ -24,35 +12,12 @@ function makeRes ( body, headers = {}, status = 200 )
     return new Response( body, { headers, status } )
 }
 
-function newUrl ( urlStr )
-{
-    try
-    {
-        return new URL( urlStr )
-    } catch ( err )
-    {
-        return null
-    }
-}
-
-function checkUrl ( u )
-{
-    for ( let i of [ URL_REGEX.regex_0, URL_REGEX.regex_1, URL_REGEX.regex_2, URL_REGEX.regex_3, URL_REGEX.regex_4, URL_REGEX.regex_5, URL_REGEX.regex_6 ] )
-    {
-        if ( u.search( i ) === 0 )
-        {
-            return true
-        }
-    }
-    return false
-}
-
 async function fetchHandler ( e )
 {
-    const req = e.request
-    const urlStr = req.url
-    const urlObj = new URL( urlStr )
+    const URL_REGEX = /^(?:https?:\/\/)?(((?:codeload|gist)?(?:\.)?github\.com)|(?:desktop|gist|github\-releases|raw|user\-images)?(?:\.)?(githubusercontent\.com))\/.*$/i
 
+    const req = e.request
+    const urlObj = new URL( req.url )
     let path = urlObj.searchParams.get( 'q' )
 
     if ( path )
@@ -62,30 +27,16 @@ async function fetchHandler ( e )
 
     path = urlObj.href.substring( urlObj.origin.length + 1 ).replace( /^https?:\/+/, 'https://' )
 
-    if ( path.search( URL_REGEX.regex_1 ) === 0 || path.search( URL_REGEX.regex_5 ) === 0 || path.search( URL_REGEX.regex_6 ) === 0 || path.search( URL_REGEX.regex_3 ) === 0 || path.search( URL_REGEX.regex_4 ) === 0 || path.search( URL_REGEX.regex_0 ) === 0 )
-    {
-        if ( path.search( URL_REGEX.regex_2 ) === 0 )
-        {
-            return httpHandler( req, path.replace( /\/(blob|edit)\//, '/raw/' ) )
-        } else
-        {
-            return httpHandler( req, path )
-        }
-    } else
+    if ( !URL_REGEX.test( path ) )
     {
         return new Response( "403 Forbidden", {
             status: 403,
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "content-type": "text/plain;charset=UTF-8",
-            },
+            headers: { "Access-Control-Allow-Origin": "*", "content-type": "text/plain;charset=UTF-8" },
         } )
     }
-}
 
-function httpHandler ( req, pathname )
-{
     const reqHdrRaw = req.headers
+    const reqInit = { body: req.body, headers: reqHdrRaw, method: req.method, redirect: 'manual' }
 
     if ( req.method === 'OPTIONS' && reqHdrRaw.has( 'Access-Control-Request-Headers' ) )
     {
@@ -94,31 +45,39 @@ function httpHandler ( req, pathname )
                 'Access-Control-Allow-Methods': 'DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT',
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Max-Age': '86400',
-            }, status: 204
+            },
+            status: 204,
         } )
     }
 
-    let rawLen = ''
-    let urlStr = pathname
+    const res = await fetch( path, reqInit )
+    const resHdrNew = new Headers( res.headers )
 
-    if ( !urlStr.match( /^https?:\/\// ) )
+    if ( resHdrNew.has( 'Location' ) )
     {
-        urlStr = 'https://' + urlStr
+        const location = resHdrNew.get( 'Location' )
+        if ( URL_REGEX.test( location ) )
+        {
+            resHdrNew.set( 'Location', '/' + location )
+        } else
+        {
+            reqInit.redirect = 'follow'
+            return proxy( reqInit, new URL( location ) )
+        }
     }
 
-    const reqHdrNew = new Headers( reqHdrRaw )
-    const urlObj = newUrl( urlStr )
-    const reqInit = {
-        body: req.body,
-        headers: reqHdrNew,
-        method: req.method,
-        redirect: 'manual'
-    }
+    resHdrNew.set( 'Access-Control-Allow-Origin', '*' )
+    resHdrNew.set( 'Access-Control-Expose-Headers', '*' )
+    resHdrNew.set( 'Cache-Control', 'no-store' )
 
-    return proxy( rawLen, reqInit, urlObj )
+    resHdrNew.delete( 'Clear-Site-Data' )
+    resHdrNew.delete( 'Content-Security-Policy' )
+    resHdrNew.delete( 'Content-Security-Policy-Report-Only' )
+
+    return new Response( res.body, { headers: resHdrNew, status: res.status } )
 }
 
-async function proxy ( rawLen, reqInit, urlObj )
+async function proxy ( reqInit, urlObj )
 {
     const res = await fetch( urlObj.href, reqInit )
     const resHdrOld = res.headers
@@ -126,42 +85,24 @@ async function proxy ( rawLen, reqInit, urlObj )
 
     if ( resHdrNew.has( 'Location' ) )
     {
-        let _location = resHdrNew.get( 'Location' )
-
-        if ( checkUrl( _location ) )
-            resHdrNew.set( 'Location', '/' + _location )
-        else
+        let location = resHdrNew.get( 'Location' )
+        if ( URL_REGEX.test( location ) )
+        {
+            resHdrNew.set( 'Location', '/' + location )
+        } else
         {
             reqInit.redirect = 'follow'
-
-            return proxy( newUrl( _location ), reqInit )
-        }
-    }
-
-    if ( rawLen )
-    {
-        const newLen = resHdrOld.get( 'Content-Length' ) || ''
-        const badLen = ( rawLen !== newLen )
-
-        if ( badLen )
-        {
-            return makeRes( res.body, {
-                '--error': `bad len: ${ newLen }, except: ${ rawLen }`,
-                'Access-Control-Expose-Headers': '--error',
-            }, 400 )
+            return proxy( reqInit, new URL( location ) )
         }
     }
 
     resHdrNew.set( 'Access-Control-Allow-Origin', '*' )
     resHdrNew.set( 'Access-Control-Expose-Headers', '*' )
-    resHdrNew.set( 'Cache-Control', 'max-age=0' )
+    resHdrNew.set( 'Cache-Control', 'no-store' )
 
     resHdrNew.delete( 'Clear-Site-Data' )
     resHdrNew.delete( 'Content-Security-Policy' )
     resHdrNew.delete( 'Content-Security-Policy-Report-Only' )
 
-    return new Response( res.body, {
-        headers: resHdrNew,
-        status: res.status
-    } )
+    return new Response( res.body, { headers: resHdrNew, status: res.status } )
 }
