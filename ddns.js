357fe894e3b5806c2cd813f549d86d0fb68b6833
Zhijie He <hezhijie0327@hotmail.com>
2024-11-22 10:37:30 +0800

    Update ddns.js

--- ddns.js
+++ ddns.js
@@ -1,22 +1,21 @@
-// Current Version: 1.0.0
+// Current Version: 1.0.1
 // Description: Using Cloudflare Workers to update your DNS record.
 
-addEventListener( "fetch", ( event ) =>
+addEventListener( "fetch", event =>
 {
     event.respondWith( handleRequest( event.request ) )
 } )
 
 async function handleRequest ( request )
 {
-    // 从请求头中获取 X-Auth-Email 和 X-Auth-Key
     const headers = request.headers
+
     const XAuthEmail = headers.get( "X-Auth-Email" ) || null
     const XAuthKey = headers.get( "X-Auth-Key" ) || null
 
-    // 解析 URL 查询参数
     const requestUrl = new URL( request.url )
-
     const params = requestUrl.searchParams
+
     const operation = ( params.get( "operation" ) || 'CREATE' ).toUpperCase()
     const recordProxy = params.get( "record_proxy" ) || 'false'
     const recordName = params.get( "record_name" ) || null
@@ -51,67 +50,112 @@ async function handleRequest ( request )
         )
     }
 
-
     let result
     switch ( operation )
     {
         case 'CREATE':
             result = await ddnsCreateRecord( XAuthEmail, XAuthKey, zoneID, recordName, recordType, recordValue, recordTTL, recordProxy === 'true' )
-            if ( result )
-            {
-                return new Response( JSON.stringify( { message: "Record created successfully." } ), { status: 200 } )
-            } else
-            {
-                return new Response( JSON.stringify( { error: "Failed to create record." } ), { status: 500 } )
-            }
+            return result
+                ? new Response( JSON.stringify( {
+                    message: "Record created successfully.",
+                    operation,
+                    zoneName,
+                    recordName,
+                    recordType,
+                    recordValue,
+                    recordTTL,
+                    recordProxy
+                } ), { status: 200 } )
+                : new Response( JSON.stringify( {
+                    error: "Failed to create record.",
+                    operation,
+                    zoneName,
+                    recordName,
+                    recordType,
+                    recordValue,
+                    recordTTL,
+                    recordProxy
+                } ), { status: 500 } )
 
         case 'UPDATE':
             const recordID = await getRecordID( XAuthEmail, XAuthKey, zoneID, recordName, recordType )
             if ( !recordID )
             {
-                return new Response( JSON.stringify( { error: "Record ID Not Found" } ), { status: 404, headers: { "Content-Type": "application/json" } } )
+                return new Response( JSON.stringify( {
+                    error: "Record ID Not Found",
+                    operation,
+                    zoneName,
+                    recordName,
+                    recordType
+                } ), { status: 404, headers: { "Content-Type": "application/json" } } )
             }
             result = await ddnsUpdateRecord( XAuthEmail, XAuthKey, zoneID, recordID, recordName, recordType, recordValue, recordTTL, recordProxy === 'true' )
-            if ( result )
-            {
-                return new Response( JSON.stringify( { message: "Record updated successfully." } ), { status: 200 } )
-            } else
-            {
-                return new Response( JSON.stringify( { error: "Failed to update record." } ), { status: 500 } )
-            }
+            return result
+                ? new Response( JSON.stringify( {
+                    message: "Record updated successfully.",
+                    operation,
+                    zoneName,
+                    recordName,
+                    recordType,
+                    recordValue,
+                    recordTTL,
+                    recordProxy
+                } ), { status: 200 } )
+                : new Response( JSON.stringify( {
+                    error: "Failed to update record.",
+                    operation,
+                    zoneName,
+                    recordName,
+                    recordType,
+                    recordValue,
+                    recordTTL,
+                    recordProxy
+                } ), { status: 500 } )
 
         case 'DELETE':
             const deleteRecordID = await getRecordID( XAuthEmail, XAuthKey, zoneID, recordName, recordType )
             if ( !deleteRecordID )
             {
-                return new Response( JSON.stringify( { error: "Record ID Not Found" } ), { status: 404, headers: { "Content-Type": "application/json" } } )
+                return new Response( JSON.stringify( {
+                    error: "Record ID Not Found",
+                    operation,
+                    zoneName,
+                    recordName,
+                    recordType
+                } ), { status: 404, headers: { "Content-Type": "application/json" } } )
             }
             result = await ddnsDeleteRecord( XAuthEmail, XAuthKey, zoneID, deleteRecordID )
-            if ( result )
-            {
-                return new Response( JSON.stringify( { message: "Record deleted successfully." } ), { status: 200 } )
-            } else
-            {
-                return new Response( JSON.stringify( { error: "Failed to delete record." } ), { status: 500 } )
-            }
+            return result
+                ? new Response( JSON.stringify( {
+                    message: "Record deleted successfully.",
+                    operation,
+                    zoneName,
+                    recordName,
+                    recordType
+                } ), { status: 200 } )
+                : new Response( JSON.stringify( {
+                    error: "Failed to delete record.",
+                    operation,
+                    zoneName,
+                    recordName,
+                    recordType
+                } ), { status: 500 } )
 
         default:
-            return new Response( JSON.stringify( { error: "Invalid operation." } ), { status: 400, headers: { "Content-Type": "application/json" } } )
+            return new Response( JSON.stringify( {
+                error: "Invalid operation.",
+                operation,
+                zoneName,
+                recordName
+            } ), { status: 400, headers: { "Content-Type": "application/json" } } )
     }
+
 }
 
-// 发送 POST 请求并处理响应
 async function ddnsCreateRecord ( XAuthEmail, XAuthKey, ZoneID, RecordName, Type, WANIP, TTL, ProxyStatus )
 {
     const url = `https://api.cloudflare.com/client/v4/zones/${ ZoneID }/dns_records`
-
-    const requestData = {
-        type: Type,
-        name: RecordName,
-        content: WANIP,
-        ttl: TTL,
-        proxied: ProxyStatus
-    }
+    const requestData = { type: Type, name: RecordName, content: WANIP, ttl: TTL, proxied: ProxyStatus }
 
     try
     {
@@ -124,16 +168,8 @@ async function ddnsCreateRecord ( XAuthEmail, XAuthKey, ZoneID, RecordName, Type
             },
             body: JSON.stringify( requestData )
         } )
-
         const data = await response.json()
-
-        if ( data.success === true )
-        {
-            return true
-        } else
-        {
-            return false
-        }
+        return data.success === true
     } catch ( error )
     {
         console.error( error )
@@ -141,18 +177,10 @@ async function ddnsCreateRecord ( XAuthEmail, XAuthKey, ZoneID, RecordName, Type
     }
 }
 
-// 发送 PUT 请求并处理响应
 async function ddnsUpdateRecord ( XAuthEmail, XAuthKey, ZoneID, RecordID, RecordName, Type, WANIP, TTL, ProxyStatus )
 {
     const url = `https://api.cloudflare.com/client/v4/zones/${ ZoneID }/dns_records/${ RecordID }`
-
-    const requestData = {
-        type: Type,
-        name: RecordName,
-        content: WANIP,
-        ttl: TTL,
-        proxied: ProxyStatus
-    }
+    const requestData = { type: Type, name: RecordName, content: WANIP, ttl: TTL, proxied: ProxyStatus }
 
     try
     {
@@ -165,16 +193,8 @@ async function ddnsUpdateRecord ( XAuthEmail, XAuthKey, ZoneID, RecordID, Record
             },
             body: JSON.stringify( requestData )
         } )
-
         const data = await response.json()
-
-        if ( data.success === true )
-        {
-            return true
-        } else
-        {
-            return false
-        }
+        return data.success === true
     } catch ( error )
     {
         console.error( error )
@@ -182,7 +202,6 @@ async function ddnsUpdateRecord ( XAuthEmail, XAuthKey, ZoneID, RecordID, Record
     }
 }
 
-// 发送 DELETE 请求并处理响应
 async function ddnsDeleteRecord ( XAuthEmail, XAuthKey, ZoneID, RecordID )
 {
     const url = `https://api.cloudflare.com/client/v4/zones/${ ZoneID }/dns_records/${ RecordID }`
@@ -197,54 +216,12 @@ async function ddnsDeleteRecord ( XAuthEmail, XAuthKey, ZoneID, RecordID )
                 'Content-Type': 'application/json'
             }
         } )
-
         const data = await response.json()
-
-        if ( data.success === true )
-        {
-            return true
-        } else
-        {
-            return false
-        }
+        return data.success === true
     } catch ( error )
     {
         console.error( error )
         return false
-    }
-}
-
-async function getRecordValue ( XAuthEmail, XAuthKey, ZoneID, RecordID )
-{
-    const url = `https://api.cloudflare.com/client/v4/zones/${ ZoneID }/dns_records/${ RecordID }`
-
-    try
-    {
-        // 发起请求
-        const response = await fetch( url, {
-            method: "GET",
-            headers: {
-                "X-Auth-Email": XAuthEmail,
-                "X-Auth-Key": XAuthKey,
-                "Content-Type": "application/json",
-            },
-        } )
-
-        // 解析响应
-        const data = await response.json()
-
-        // 检查 success 字段
-        if ( data.success === true )
-        {
-            return data.result.content
-        } else
-        {
-            return null
-        }
-    } catch ( error )
-    {
-        console.error( error )
-        return null
     }
 }
 
@@ -254,7 +231,6 @@ async function getRecordID ( XAuthEmail, XAuthKey, ZoneID, RecordName, RecordTyp
 
     try
     {
-        // 发起请求
         const response = await fetch( url, {
             method: "GET",
             headers: {
@@ -263,28 +239,13 @@ async function getRecordID ( XAuthEmail, XAuthKey, ZoneID, RecordName, RecordTyp
                 "Content-Type": "application/json",
             },
         } )
-
-        // 解析响应
         const data = await response.json()
-
-        // 检查 success 字段
-        if ( data.success === true && data.result.length > 0 )
+        if ( data.success && data.result.length > 0 )
         {
             const record = data.result.find( record => record.type === RecordType )
-            if ( record )
-            {
-                return record.id // 返回符合条件的记录 ID
-            } else
-            {
-                return null
-            }
-        } else if ( data.success === false )
-        {
-            return null
-        } else
-        {
-            return null
+            return record ? record.id : null
         }
+        return null
     } catch ( error )
     {
         console.error( error )
@@ -298,7 +259,6 @@ async function getZoneID ( XAuthEmail, XAuthKey, ZoneName )
 
     try
     {
-        // 发起请求
         const response = await fetch( url, {
             method: "GET",
             headers: {
@@ -307,21 +267,8 @@ async function getZoneID ( XAuthEmail, XAuthKey, ZoneName )
                 "Content-Type": "application/json",
             },
         } )
-
-        // 解析响应
         const data = await response.json()
-
-        // 检查 success 字段
-        if ( data.success === true && data.result.length > 0 )
-        {
-            return data.result[ 0 ].id
-        } else if ( data.success === false )
-        {
-            return null
-        } else
-        {
-            return null
-        }
+        return data.success && data.result.length > 0 ? data.result[ 0 ].id : null
     } catch ( error )
     {
         console.error( error )
@@ -335,7 +282,6 @@ async function getAccountName ( XAuthEmail, XAuthKey )
 
     try
     {
-        // 发起请求
         const response = await fetch( url, {
             method: "GET",
             headers: {
@@ -344,21 +290,8 @@ async function getAccountName ( XAuthEmail, XAuthKey )
                 "Content-Type": "application/json",
             },
         } )
-
-        // 解析响应
         const data = await response.json()
-
-        // 检查 success 字段
-        if ( data.success === true && data.result.length > 0 )
-        {
-            return data.result[ 0 ].name
-        } else if ( data.success === false )
-        {
-            return null
-        } else
-        {
-            return null
-        }
+        return data.success && data.result.length > 0 ? data.result[ 0 ].name : null
     } catch ( error )
     {
         console.error( error )
