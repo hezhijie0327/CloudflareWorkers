5c2dbe34da57b0482de232a86592295d51cf56d3
Zhijie He <hezhijie0327@hotmail.com>
2024-11-22 11:34:25 +0800

    fonts.js | Version 1.0.6

--- fonts.js
+++ fonts.js
@@ -1,7 +1,7 @@
-// Current Version: 1.0.5
+// Current Version: 1.0.6
 // Description: Using Cloudflare Workers to speed up fonts.googleapis.com and fonts.gstatic.com's visting.
 
-addEventListener( "fetch", ( event ) =>
+addEventListener( "fetch", event =>
 {
     event.respondWith( handleRequest( event.request ) )
 } )
@@ -9,13 +9,13 @@ addEventListener( "fetch", ( event ) =>
 async function handleRequest ( request )
 {
     let url = request.url.substr( 8 )
-    let path = url.split( "/" )
+    let path = url.split( "/" )[ 0 ]
     url = url.substr( url.indexOf( "/" ) + 1 )
 
-    var response_css = await fetch( "https://fonts.googleapis.com/" + url )
-    var response_font = await fetch( "https://fonts.gstatic.com/" + url )
+    const response_css = await fetch( "https://fonts.googleapis.com/" + url )
+    const response_font = await fetch( "https://fonts.gstatic.com/" + url )
 
-    if ( url === "" || ( response_css.status !== 200 && response_font.status !== 200 ) )
+    if ( !url || ( response_css.status !== 200 && response_font.status !== 200 ) )
     {
         return new Response( "404 Not Found", {
             status: 404,
@@ -24,100 +24,48 @@ async function handleRequest ( request )
                 "content-type": "text/plain;charset=UTF-8",
             },
         } )
-    } else
-    {
-        if ( response_css.status === 200 )
-        {
-            let css = await response_css.text()
+    }
 
-            return new Response( css.replace( /fonts\.gstatic\.com/gim, path[ 0 ] ), {
+    if ( response_css.status === 200 )
+    {
+        let css = await response_css.text()
+        css = css.replace( /fonts\.gstatic\.com/gim, path )
+        return new Response( css, {
+            status: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "content-type": "text/css;charset=UTF-8",
+            },
+        } )
+    }
+
+    const fontExtMap = {
+        ".collection": "font/collection;charset=UTF-8",
+        ".eot": "application/vnd.ms-fontobject;charset=UTF-8",
+        ".otf": "font/otf;charset=UTF-8",
+        ".sfnt": "font/sfnt;charset=UTF-8",
+        ".svg": "image/svg+xml;charset=UTF-8",
+        ".ttf": "font/ttf;charset=UTF-8",
+        ".woff": "font/woff;charset=UTF-8",
+        ".woff2": "font/woff2;charset=UTF-8",
+    }
+
+    for ( const ext in fontExtMap )
+    {
+        if ( url.match( new RegExp( `${ ext }$` ) ) )
+        {
+            return new Response( response_font.body, {
                 status: 200,
                 headers: {
                     "Access-Control-Allow-Origin": "*",
-                    "content-type": "text/css;charset=UTF-8",
+                    "content-type": fontExtMap[ ext ],
                 },
             } )
-        } else
-        {
-            if ( url.match( /\.collection$/ ) )
-            {
-                return new Response( response_font.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "font/collection;charset=UTF-8",
-                    },
-                } )
-            } else if ( url.match( /\.eot$/ ) )
-            {
-                return new Response( response_font.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "application/vnd.ms-fontobject;charset=UTF-8",
-                    },
-                } )
-            } else if ( url.match( /\.otf$/ ) )
-            {
-                return new Response( response_font.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "font/otf;charset=UTF-8",
-                    },
-                } )
-            } else if ( url.match( /\.sfnt$/ ) )
-            {
-                return new Response( response_font.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "font/sfnt;charset=UTF-8",
-                    },
-                } )
-            } else if ( url.match( /\.svg$/ ) )
-            {
-                return new Response( response_font.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "image/svg+xml;charset=UTF-8",
-                    },
-                } )
-            } else if ( url.match( /\.ttf$/ ) )
-            {
-                return new Response( response_font.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "font/ttf;charset=UTF-8",
-                    },
-                } )
-            } else if ( url.match( /\.woff$/ ) )
-            {
-                return new Response( response_font.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "font/woff;charset=UTF-8",
-                    },
-                } )
-            } else if ( url.match( /\.woff2$/ ) )
-            {
-                return new Response( response_font.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "font/woff2;charset=UTF-8",
-                    },
-                } )
-            } else
-            {
-                return new Response( response_font.body, {
-                    status: 200,
-                    headers: response_font.headers,
-                } )
-            }
         }
     }
+
+    return new Response( response_font.body, {
+        status: 200,
+        headers: response_font.headers,
+    } )
 }
